6ebd0e176bcd92bb109379953e14c960e89f0952
taracdia <36205314+taracdia@users.noreply.github.com>
2019-03-17 10:38:36 -0500

    setup finished

--- javascript.js
+++ javascript.js
@@ -7,151 +7,184 @@ $(document).ready(function() {
   // TODO: handle situation where they have no faves yet
   // TODO: handle isOnFave being true and then immediately calling getFaves because the user id wouldn't be set yet
 
-
+  /*
+  userID has three modes: unset, empty, and set
+    unset: undefined. only happens on first opening of page. Afterwards, a function asks the browser what the userID is. If the user is not signed in, userID is set to an empty string. Otherwise UserID is set to a unique UserID.
+    empty: empty string. Means user is not logged in to Chrome
+    set: the user's ID
+  */
 
 for (var i = 0; i < 10; i++){
   $("#multipleCatsDiv").append($("<div class=\"catImageDiv\"></div>"));
 }
 
-/*
-userID has three modes: unset, empty, and set
-  unset: undefined. only happens on first opening of page. Afterwards, a function asks the browser what the userID is. If the user is not signed in, userID is set to an empty string. Otherwise UserID is set to a unique UserID.
-  empty: empty string. Means user is not logged in to Chrome
-  set: the user's ID
+//-----Set up with local storage-----
+  if (typeof localStorage.userID !== "undefined"){
+    //run the function to check for ID from browser
+    //if it's empty:
+      //disable the things that get disabled when they don't have a UserID
+    //else:
+      //save the ID to userID in localStorage
+  }
 
-local storage on open:
-  multipleBoolean:
-    if true: multiMode();
-    if false: singleMode();
-    if not set: singleMode();
-  faveBoolean:
-    if true: getFaveOnClick;
-    if false: getRandOnClick;
-    if not set: getRandOnClick;
-  numberOfCats: apply to number input or leave blank if not set or set to 0
-  imageSize: apply to imageSizeSlider
-  userID:
-    if one is saved, set it to userID global variable
-    else: run the function that gets the userID and set it to userID
+  if(localStorage.multipleBoolean){
+    multiMode();
+  } else {
+    singleMode();
+  }
 
-local storage on close:
-  multipleBoolean:
-    true if: numberMode is "many"
-    else false
-  faveBoolean:
-    set as isOnFave
-  numberOfCats: apply to number input, default 0 if not there
-  imageSize: from imageSizeSlider
-  userID: save userID
+if(localStorage.numberOfCats){
+  $("#catsNumberInput").val(localStorage.numberOfCats);
+}
 
+if(localStorage.imageSize){
+  $("#imageSizeSlider").val(localStorage.imageSize);
+}
 
-radio listener:
-  when changed, run singleMode() or run multiMode()
+if(localStorage.faveBoolean){
+  getFaveOnClick();
+} else {
+  getRandOnClick();
+}
 
-slider listener:
-  when changed, changeImageSize()
+changeImageSize();
+//-----temp-------
+console.log("imageSize is " + localStorage.imageSize);
+console.log("faveBoolean is " + localStorage.faveBoolean);
+console.log("numberOfCats is " + localStorage.numberOfCats);
+console.log("userID is " + localStorage.userID);
+console.log("multipleBoolean is " + localStorage.multipleBoolean);
+$("#resetCookies").click(function(){
+  localStorage.clear();
+})
+
+//--Listeners--
+$("#imageSizeSlider").change(function(){
+  localStorage.imageSize = $("#imageSizeSlider").val();
+  changeImageSize();
+});
+
+$("input[name=numberMode]:radio").change(function(){
+  if ($(this).val() === "one"){
+    singleMode();
+  } else {
+    multiMode();
+  }
+});
 
 function singleMode(){
-  howManyCatsSlider hide
-  sizeSlider hide
-  make sure radio "number" is "one" and not "many"
-  show singleCatDiv
-  hide multipleCatsDiv
-  hide catsNumberInput
+
+//     change text of getFaves and newCAt buttons to singular
+//     save local storage multipleBoolean: false
+//   howManyCatsSlider hide
+//   sizeSlider hide
+//   make sure radio "number" is "one" and not "many"
+//   show singleCatDiv
+//   hide multipleCatsDiv
+//   hide catsNumberInput
 }
 
 function multiMode(){
-  howManyCatsSlider show
-  sizeSlider show
-  make sure radio "number" is "many" and not "one"
-  hide singleCatDiv
-  show multipleCatsDiv
-  show catsNumberInput
+    //change text of getFaves and newCAt buttons to multiple
+    //save local storage multipleBoolean: true
+//   howManyCatsSlider show
+//   sizeSlider show
+//   make sure radio "number" is "many" and not "one"
+//   hide singleCatDiv
+//   show multipleCatsDiv
+//   show catsNumberInput
 }
 
-getFaveOnClick:
-  isOnFave = true;
-  setDivs(shuffleArray(getFaveCats(getCatLimit())));
+function getFaveOnClick(){
+  //   isOnFave = true;
+        //save local storage faveBoolean: true
+  //   setDivs(shuffleArray(getFaveCats(getCatLimit())));
+  //changeImageSize()
+}
 
-getRandOnClick:
-  isOnFave = false;
-  setDivs(getRandomCats(getCatLimit()));
+function getRandOnClick(){
+  //   isOnFave = false;
+      //save local storage faveBoolean: false
+  //   setDivs(getRandomCats(getCatLimit()));
+  //changeImageSize()
+}
 
 function setDivs(array){
-  singleCatDiv.innerHTML = ""
-  multipleCatsDiv.innerHTML = ""
-  createResponsiveDiv(singleCatDiv, array[0]);
-
-  for (var i = 0; i < array.length; i++){
-    create div
-    createResponsiveDiv(div, array[i]);
-    add div to multipleCatsDiv
-    add class="catImageDiv" to div
-  }
+//   singleCatDiv.innerHTML = ""
+//   multipleCatsDiv.innerHTML = ""
+//   createResponsiveDiv(singleCatDiv, array[0]);
+//
+//   for (var i = 0; i < array.length; i++){
+//     create div
+//     createResponsiveDiv(div, array[i]);
+//     add div to multipleCatsDiv
+//     add class="catImageDiv" to div
+//   }
 }
 
 function createResponsiveDiv(div, cat){
-  add button to div
-  add img to div
-  img add src as cat["url"];
-  img add id as cat["id"];
-  if cat["fave_id"] exists:
-    set it to img's alt
-    button is delete type**
-  else:
-    set img's alt to empty string
-    button is addFaves type**
+//   add button to div
+//   add img to div
+//   img add src as cat["url"];
+//   img add id as cat["id"];
+//   if cat["fave_id"] exists:
+//     set it to img's alt
+//     button is delete type**
+//   else:
+//     set img's alt to empty string
+//     button is addFaves type**
 }
 
 function getRandomCats(catLimit){
-  var catsArray = [];
-  {
-    //pass sub_id IF it is set
-    get a random cat
-    if it has include_favourite = 1:
-      skip
-    else:
-      add it to catsArray
-  } do while catsArray.length < catLimit
-  return catsArray;
+//   var catsArray = [];
+//   {
+//     //pass sub_id IF it is set
+//     get a random cat
+//     if it has include_favourite = 1:
+//       skip
+//     else:
+//       add it to catsArray
+//   } do while catsArray.length < catLimit
+//   return catsArray;
 }
 
 function getFaveCats(catLimit){
-  var outputArray = [];
-  var inputArray = getFaveArray(catLimit);
-  for each cat in inputArray:{
-    make object;
-    object["fave_id"] = cat["id"];
-    object["id"] = cat["image"]["id"];
-    object["url"] = cat["image"]["url"];
-
-    add object to outputArray
-  }
-  return outputArray;
+//   var outputArray = [];
+//   var inputArray = getFaveArray(catLimit);
+//   for each cat in inputArray:{
+//     make object;
+//     object["fave_id"] = cat["id"];
+//     object["id"] = cat["image"]["id"];
+//     object["url"] = cat["image"]["url"];
+//
+//     add object to outputArray
+//   }
+//   return outputArray;
 }
 
 function shuffleArray(array){
-  return shuffledArray;
+//   return shuffledArray;
 }
 
 function getFaveArray(catLimit){
-  //need to put in limit parameter
-  //API
-  return faveArray
+//   //need to put in limit parameter
+//   //API
+//   return faveArray
 }
 
 function getCatLimit(){
-  if there is no number in catsNumberInput:
-    return 10;
-  else:
-    return catsNumberInput number
+    //var catNumber = 10;
+//   if there is a number in catsNumberInput{
+//     catnumber = catsNumberInput number;}
+  //save catNumber in local storage
+  //return catNumber;
 }
 
 function changeImageSize(){
-  var dimensions = get slider value
-  change css of imageCard class to height = dimensions, width = dimensions
+//   var dimensions = get slider value
+//   change css of imageCard class to height = dimensions, width = dimensions
 }
-*/
+
 
 
 
